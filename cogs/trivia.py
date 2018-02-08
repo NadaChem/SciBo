f5e00a98887e7caece1a5663821c8261c2cd95ed
James <naught0@github.com>
2018-02-08 18:01:48 -0500

    preliminary testing for trivia

---
+++ cogs/trivia.py
@@ -0,0 +1,36 @@
+import asyncio
+import json
+import random
+
+from discord.ext import commands
+from typing import Dict
+
+
+class Trivia:
+    """A guessing game based on chemical formulas.
+    
+    Attributes:
+        bot (commands.Bot): the bot
+
+        CHEM_DATA (Dict[str, str]): Chemical name -> notation
+    """
+    with open('../data/chem.json') as f:
+        CHEM_DATA = json.load(f)
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def trivia(self, ctx):
+        """Start a guessing game for chemical compositions.
+
+        SciBo will ask you the chemical notation for a random chemical.
+        If you guess it, you get a point!
+        """
+        rand_key = random.choice(list(self.CHEM_DATA))
+        answer = self.CHEM_DATA[rand_key]
+
+        async def on_message(message):
+            """Collect the messages"""
+            if message.content == answer:
+                return await ctx.send(f'Correct! {message.author.mention}. The answer was `{answer}`.')
