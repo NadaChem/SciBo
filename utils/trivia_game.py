8ab6133ca898a37a271dc919fed3290af957113a
James <naughtsee@gmail.com>
2018-02-09 19:46:57 -0500

    add some docstrings for clarification and debugging

--- utils/trivia_game.py
+++ utils/trivia_game.py
@@ -5,6 +5,23 @@ from typing import Union
 
 
 class Game:
+    """A class representing a ScIeNtiFic trivia game
+
+    Attributes:
+        ctx: The context in which the command was called
+        bot: The discord.ext.commands.Bot instance currently running
+        loop: The bot's asyncio loop
+        guild_id: The server id of in which the :class:`Game`is currently
+            being run
+        count: A running tally of how many questions have been asked
+        limit: The # of questions to be asked in total (can be None for unlimited)
+        task: :meth:`trivia_task`
+
+    Args:
+        ctx: The context in which the command was called (passed from the cog)
+        limit: The # of questions to be asked (passed from cog)
+    """
+
     def __init__(self, ctx, *, limit: Union[int, None]):
         self.ctx = ctx
         self.bot = ctx.bot
@@ -15,6 +32,9 @@ class Game:
         self.task = self.loop.create_task(self.trivia_task())
 
     async def trivia_task(self):
+        """The trivia task.
+        Runs the trivia game in a loop.
+        Exits if stop() is called, or if the limit of the game is reached."""
         while not self.bot.is_closed():
             rand_key = random.choice(list(self.bot.CHEM_DATA))
             answer = self.bot.CHEM_DATA[rand_key]
@@ -37,10 +57,17 @@ class Game:
             if self.count > self.limit:
                 return self.stop()
 
+            # Less spammy
+            await asyncio.sleep(3)
+
+            # TODO:
+            # This portion isn't being reached
+            # I believe it has something to do with the break statements above
             await self.ctx.send('Next question in 10 seconds!')
             await asyncio.sleep(10)
 
     async def stop(self):
+        """Stops the asyncio trivia task"""
         if not self.task.cancelled():
             await self.ctx.send(f"Fun's over! You answered {self.count} questions.")
             return self.task.cancel()
